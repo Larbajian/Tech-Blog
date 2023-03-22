b8ef0bbbc014bfda684373f3565c229e7041203e
unknown <lexiarbajianclt@gmail.com>
2023-03-21 20:22:02 -0400

    rearranging files

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const exphbs = require('express-handlebars');
 const routes = require('./Main/controllers');
 const helpers = require('./views/layouts/utils/helpers');
 
-const sequelize = require('./config/connection');
+const sequelize = require('./Main/config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
